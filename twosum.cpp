8be53026811791660c837e4ae7a8c7ddf520f6fb
Nathalie Himaya <natramoshi@gmail.com>
2025-08-23 22:44:02 -0700

    Solved TwoSum

--- twosum.cpp
+++ twosum.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> target_index;
+        vector<int> target_index;   // stores the final answer (two indices)
 
-        unordered_map<int, int> hash_table;
-        for(int i = 0; i < nums.size(); i++){
-            int second_num = target - nums.at(i);
+        unordered_map<int, int> hash_table; // key = element from nums, value = its index
 
-            if (hash_table.find(second_num) != hash_table.end()){
-                target_index.push_back(i);
+        // loop through each element in nums
+        for (int i = 0; i < nums.size(); i++) {
+            int second_num = target - nums.at(i); // the "complement" we need
+
+            // IF: check if complement is already in hash_table
+            if (hash_table.find(second_num) != hash_table.end()) {
+                // Found the complement → solution exists
+                // push the index of the complement (value stored in hash map)
                 target_index.push_back(hash_table.find(second_num)->second);
-                break;
+                // push the current index
+                target_index.push_back(i);
+                break; // stop, because we only need one valid pair
             }
             else {
+                // ELSE: complement not found → store current number for future
+                // key = number, value = index
                 hash_table[nums.at(i)] = i;
             }
         }
-        return target_index;
+
+        return target_index; // return the two indices that add up to target
     }
 };
